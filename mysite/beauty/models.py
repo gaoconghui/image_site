e75051eb7c0b45449c8bdb113d4a61eae13cf5d7
gaoconghui <381147882@qq.com>
2017-06-11 15:46:22 +0800

    更改数据库表结构

--- mysite/beauty/models.py
+++ mysite/beauty/models.py
@@ -4,17 +4,22 @@ from django.db import models
 
 class Gallery(models.Model):
     gallery_id = models.CharField(max_length=200)
-    publish_time = models.IntegerField()
-    insert_time = models.IntegerField()
-    pages = models.IntegerField()
-    tags = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    domain = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200,null=True)
+    from_id = models.CharField(max_length=200)
+    publish_time = models.IntegerField(null=True)
+    insert_time = models.IntegerField(null=True)
+    all_page = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.question_text
+        return self.title
 
 
 class Image(models.Model):
     gallery_id = models.CharField(max_length=200)
-    image_id = models.CharField(max_length=200)
+    image_id = models.CharField(max_length=200,null=True)
+    image_url = models.CharField(max_length=200)
+    title = models.CharField(max_length=200,null=True)
+    desc = models.CharField(max_length=200,null=True)
     order = models.IntegerField()
-
