fb54d3fb89fc1fbd6981460800db0e98bd38e830
gaoconghui <381147882@qq.com>
2017-06-13 21:05:02 +0800

    add detail page

--- mysite/beauty/urls.py
+++ mysite/beauty/urls.py
@@ -7,8 +7,8 @@ app_name = 'beauty'
 urlpatterns = [
     # ex: /beauty/
     url(r'^$', views.index, name='index'),
-    url(r'^gallery/(?P<gallery_id>\w+)/$', views.gallery, name='gallery'),
     url(r'^(?P<page_num>\d+)/$', views.index, name='hot'),
+    url(r'^gallery/(?P<_id>\w+)/(?P<page_num>\d+)$', views.gallery, name='gallery'),
     url(r'^(?P<tag_name>\w+)/(?P<page_num>\d+)/$', views.tag_page, name='tag_page'),
 # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
