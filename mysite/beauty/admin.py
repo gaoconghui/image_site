9dc31df2200b54e01be3c1a04bdbcbfe14acef4b
gaoconghui <381147882@qq.com>
2017-06-24 20:31:58 +0800

    增加管理model

--- mysite/beauty/admin.py
+++ mysite/beauty/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from beauty.models import Gallery, Tag, Image
+
+admin.site.register(Gallery)
+admin.site.register(Tag)
+admin.site.register(Image)
