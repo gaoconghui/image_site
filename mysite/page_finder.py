2026694d09bef8a59435c2fe2f5abbe92b95e776
gaoconghui <381147882@qq.com>
2017-07-15 00:39:57 +0800

    site map 分页

--- mysite/page_finder.py
+++ mysite/page_finder.py
@@ -52,15 +52,30 @@ def gen_site_map():
         return "<url><loc>{url}</loc><priority>{priority}</priority><changefreq>{changefreq}</changefreq></url>".format(
             url=url, priority=priority, changefreq=changefreq
         )
+    def items_split(items,single_size=10000):
+        """
+        讲items分割为一组最多single_size个
+        :param single_size:
+        :return:
+        """
+        size = len(items)
+        count = size / single_size + 1
+        result = []
+        for c in range(count):
+            result.append(items[count * single_size : (count + 1) * single_size])
+        return result
 
     index_items = [gen_url_item("http://www.meizibar.com",1.0,"always")]
     tag_items = [gen_url_item(url,0.8,"daily") for url in get_all_tag_page()]
     gallery_items = [gen_url_item(url,0.6,'weekly') for url in get_all_gallery_page()]
     gallery_items_more = [gen_url_item(url,0.6,'weekly') for url in get_all_gallery_more_page()]
-    xml = base.format(items="\n".join(itertools.chain(index_items,tag_items,gallery_items,gallery_items_more)))
-    with open(os.path.join(STATIC_ROOT,"beauty_site_map.xml"),"w") as f:
-        print STATIC_ROOT
-        f.write(xml)
+    items_list = items_split(list(itertools.chain(index_items,tag_items,gallery_items,gallery_items_more)),single_size=10000)
+    for index,items in enumerate(items_list):
+        xml = base.format(items="\n".join(items))
+        file_path = os.path.join(STATIC_ROOT,"beauty_site_map{c}.xml".format(c=index))
+        with open(file_path,"w") as f:
+            print file_path
+            f.write(xml)
 
 
 if __name__ == '__main__':
